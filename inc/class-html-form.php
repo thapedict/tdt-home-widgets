156a5dd65dba5fe7cd7ca1f95bdca5743339d15f
Thapelo Moeti <benzitto@gmail.com>
2018-12-19 18:09:26 +0200

    Major Commit
    1. Can now set action property in constructor.
    2. Refractored setting of input and textarea defaults.
    3. Added LOTS & LOTS of doccomments.
    4. add_input now only supports 3 types of arrays.
    5. Added get_field and get_all_fields functions
    6. Refractored some code (most noteably on inline if statement by wrapping them with brackets)

--- inc/class-html-form.php
+++ inc/class-html-form.php
@@ -86,37 +86,43 @@ class html_form {
      *    the constructor
      *    @param array with form options
      */
-    function __construct( array $options = array() ) {
+    public function __construct( array $options = array() ) {
         if( isset( $options[ 'id' ] ) )
             $this->id = $options[ 'id' ];
 
         if( isset( $options[ 'method' ] ) )
             $this->method = $options[ 'method' ];
 
+        if( isset( $options[ 'action' ] ) )
+            $this->action = $options[ 'action' ];
+
         if( isset( $options[ 'is_multipart' ] ) )
             $this->is_multipart = $options[ 'is_multipart' ];
 
         $input_defaults = $textarea_defaults = array();
 
+        // input defaults
+        $_input_defaults = array();
+        if( isset( $options[ 'input_defaults' ] ) && is_array( $options[ 'input_defaults' ] ) )
+            $_input_defaults = array_intersect_key( $options[ 'input_defaults' ], $this->input_defaults );
+
+        $this->input_defaults = array_merge( $this->input_defaults, $_input_defaults );
 
         // label
         if( isset( $options[ 'sr_only' ] ) )
             $this->input_defaults[ 'sr_only' ] = $options[ 'sr_only' ];
 
-        // input defaults
         if( isset( $options[ 'wrap_fields' ] ) )
             $this->input_defaults[ 'wrap_field' ] = $options[ 'wrap_fields' ];
 
-        if( isset( $options[ 'input_defaults' ] ) && is_array( $options[ 'input_defaults' ] ) )
-            $input_defaults = $options[ 'input_defaults' ];
-
-        $this->input_defaults = array_merge( $this->input_defaults, $this->input_defaults, $input_defaults );
-
         // textarea defaults
-        if( isset( $options[ 'textarea_defaults' ] ) && is_array( $options[ 'textarea_defaults' ] ) )
-            $textarea_defaults = $options[ 'textarea_defaults' ];
+        $this->textarea_defaults = array_merge( $this->input_defaults, $this->textarea_defaults );
 
-        $this->textarea_defaults = array_merge( $this->input_defaults, $this->textarea_defaults, $textarea_defaults );
+        $_textarea_defaults = array();
+        if( isset( $options[ 'textarea_defaults' ] ) && is_array( $options[ 'textarea_defaults' ] ) )
+            $_textarea_defaults = array_intersect_key( $options[ 'textarea_defaults' ], $this->textarea_defaults );
+
+        $this->textarea_defaults = array_merge( $this->textarea_defaults, $_textarea_defaults );
 
         // submit defaults
         $this->submit_defaults = array_merge( $this->input_defaults, $this->submit_defaults );
@@ -127,14 +133,23 @@ class html_form {
 
     }
 
+    /**
+     *  When this class is used as a string
+     *
+     *  @see html_form::get_form
+     *
+     *  @return string the HTML version of the form
+     */
     public function __toString() {
         return $this->get_form();
     }
 
     /**
+     *  Get the HTML string of the form
      *
+     *  @return string the HTML version of the form
      */
-    function get_form() {
+    public function get_form() {
         if( empty( $this->fields ) )
             return '';
 
@@ -143,7 +158,7 @@ class html_form {
         // fields
         foreach( $this->fields as $f ) {
             switch( $f[ 'type' ] ) {
-                case in_array( $f[ 'type'], array( 'text', 'hidden','email', 'search', 'password', 'url', 'radio', 'checkbox', 'number' ) ):
+                case in_array( $f[ 'type'], $this->input_types ):
                     $form .= $this->get_input( $f );
                 break;
 
@@ -159,16 +174,20 @@ class html_form {
     }
 
     /**
-     *
+     *  Print out the HTML form
      */
-    function print_form() {
+    public function print_form() {
         print $this->get_form();
     }
 
     /**
+     *  Get HTML string of an input
      *
+     *  @param array $input properties to use when building the input.
+     *
+     *  @return string the HTML version of the input
      */
-    function get_input( array $input ) {
+    public function get_input( array $input ) {
         $string = '';
 
         if( isset( $input[ 'repeat' ] ) && is_int( $input[ 'repeat' ] ) ) {
@@ -205,11 +224,15 @@ class html_form {
 
         return $string;
     }
-
+    
     /**
+     *  Get HTML string of an textarea
      *
+     *  @param array $textarea properties to use when building the textarea.
+     *
+     *  @return string the HTML version of the textarea
      */
-    function get_textarea( array $textarea ) {
+    public function get_textarea( array $textarea ) {
         $string = '';
 
         if( isset( $textarea[ 'repeat' ] ) && is_int( $textarea[ 'repeat' ] ) ) {
@@ -248,9 +271,15 @@ class html_form {
     }
 
     /**
+     *  Wrap an input field
      *
+     *  @param string $wrap_with The tag to wrap with.
+     *  @param string $string The input field to wrap.
+     *  @param array $attr Attributes to add to the wrapping tag.
+     *
+     *  @return string Wrapped content
      */
-    function wrap( $wrap_with, $string, $attr = null ) {
+    public function wrap( $wrap_with, $string, $attr = null ) {
         $attributes = '';
         if( is_array( $attr ) ) {
             foreach( $attr as $k => $v ) {
@@ -269,11 +298,14 @@ class html_form {
     }
 
     /**
-     *    gets the  html label, or just the text
-     *    @param input array
-     *    @return string with label text
+     *  Gets the  html label, or just the text
+     *
+     *  @param array $input The input to extract the label from.
+     *  @param bool $text_only whether to return the text only or an html element.
+     *
+     *  @return string with label text
      */
-    function get_label( array $input, $text_only = false ) {
+    public function get_label( array $input, $text_only = false ) {
         $label = '';
 
         // filter to only fields that can have labels
@@ -311,74 +343,71 @@ class html_form {
     }
 
     /**
-     *    gets the placeholder text
-     *    @param input array
+     *    Gets the placeholder text
+     *
+     *    @param array $input Properties to use to get placeholder.
+     *
      *    @return string with placeholder text
      */
     function get_placeholder( array $input ) {
         $placeholder = '';
 
-        // filter to only fields that can have placeholders
-        if( in_array( $input[ 'type' ], array( 'hidden', 'submit', 'reset', 'radio', 'checkbox' ) ) )
+        // filter out only fields that can't have placeholders
+        if( in_array( $input[ 'type' ], array( 'hidden', 'submit', 'reset', 'radio', 'checkbox' ) ) ) {
             return $placeholder;
+        }
 
-        if( ! empty( $placeholder ) )
+        if( ! empty( $input[ 'placeholder' ] ) ) {
             $placeholder = sprintf( ' placeholder="%s"', $input[ 'placeholder' ] );
-        else if( $input[ 'label_is_placeholder' ] )
+        } else if( $input[ 'label_is_placeholder' ] ) {
             $placeholder = sprintf( ' placeholder="%s"', $this->get_label( $input, true )  );
+        }
 
         return $placeholder;
     }
 
     /**
-     *    adds an input to the form
-     *    @param    input name or array with input properties
-     *    @return $this
+     *    Adds an input to the form
+     *
+     *    @param string|array $input Name or array with input properties.
+     *
+     *    @return html_form $this
      */
-    function add_input( $input ) {
+    public function add_input( $input ) {
         if( is_string( $input ) ) {
-            $this->fields[] = array_merge( $this->input_defaults, array( 'name' => $input ) );
+            $this->fields[ $input ] = array_merge( $this->input_defaults, array( 'name' => $input ) );
         } else if( is_array( $input ) ) {
+            // we assume you are passing multiple inputs
             // types of arrays to expect
-            // 1. array( 'name', 'password' );
-            // 2. array( array( 'name' => 'name' ), array( 'name' => 'password' ) )
-            // 3. array( 'name' => 'name', 'required' => true );
-            // because the input name can be the array key
-            // 4. array( 'name' => array( 'required' => true ) );
-            // 5. array( 'name' => array( 'required' => true ), 'password' => array( 'required' => true ) );
+            // 1. array( 'username', 'password' );
+            // 2. array( array( 'name' => 'username' ), array( 'name' => 'password' ) )
+            // 3. array( 'username' => array( 'required' => true ), 'password' => array( 'type' => 'password', 'required' => true ) );
             foreach( $input as $k => $v ) {
                 if( is_int( $k ) ) { // 1, 2
-                    if( is_string( $v ) ) // 1
-                        $this->fields[] = array_merge( $this->input_defaults, array( 'name' => $v ) );
-                    else if ( is_array( $v ) ) { // 2
+                    if( is_string( $v ) ) { // 1
+                        $this->fields[ $v ] = array_merge( $this->input_defaults, array( 'name' => $v ) );
+                    }  else if ( is_array( $v ) ) { // 2
                         if( isset( $v[ 'name' ] ) ) {
-                            if( isset( $v[ 'type' ] ) && $v[ 'type' ] == 'textarea' )
-                                $this->fields[] = array_merge( $this->textarea_defaults, $v );
-                            else
-                                $this->fields[] = array_merge( $this->input_defaults, $v );
-                        } else {
-                            if( class_exists( 'tbt_debug' ) )
-                                tbt_debug()->log( 'Error: No Name Set' );
+                            if( isset( $v[ 'type' ] ) && $v[ 'type' ] == 'textarea' ) {
+                                $this->fields[ $v[ 'name' ] ] = $this->textarea_defaults( $v );
+                            } else {
+                                $this->fields[ $v[ 'name' ] ] = $this->input_defaults( $v );
+                            }
                         }
                     }
-                } else { // 3, 4, 5
-                    if( is_string( $v ) ) { // we assume it's 3
-                        if( isset( $input[ 'name' ] ) ) {
-                            if( isset( $input[ 'type' ] ) && $input[ 'type' ] == 'textarea' )
-                                $this->fields[] = array_merge( $this->textarea_defaults, $input );
-                            else
-                                $this->fields[] = array_merge( $this->input_defaults, $input );
+                } else if( is_string( $k ) ) { // 3
+                    if( is_array( $v ) ) {
+                        $v[ 'name' ] = $k;
+
+                        if( isset( $v[ 'type' ] ) && $v[ 'type' ] == 'textarea' ) {
+                            $this->fields[ $k ] = $this->textarea_defaults( $v );
+                        } else {
+                            $this->fields[ $k ] = $this->input_defaults( $v );
                         }
+                    } else {
+                        $v = array( 'name' => $k );
 
-                        break; // stop looping through the properties
-                    } else if( is_array( $v ) ) { // 4, 5
-                        if( ! isset( $v[ 'name' ] ) )
-                            $v[ 'name' ] = $k;
-
-                        if( isset( $v[ 'type' ] ) && $v[ 'type' ] == 'textarea' )
-                            $this->fields[] = array_merge( $this->textarea_defaults, $v );
-                        else
-                            $this->fields[] = array_merge( $this->input_defaults, $v );
+                        $this->fields[ $k ] = $this->input_defaults( $v );
                     }
                 }
             }
@@ -387,7 +416,12 @@ class html_form {
         return $this;
     }
 
-    function print_input( $input ) {
+    /**
+     *  Prints out an input html
+     *
+     *  @param string|array $input String as the ID of input or array with new input properties
+     */
+    public function print_input( $input ) {
         $args = func_get_args();
 
         foreach( $args as $input ) {
@@ -407,7 +441,12 @@ class html_form {
         }
     }
 
-    function get_post() {
+    /**
+     *  Gets form field from $_POST or $_GET
+     *
+     *  @return array|false Array with matching form fields or false on failure
+     */
+    public function get_post() {
         $post = array();
 
         foreach( $this->fields as $f ) {
@@ -451,45 +490,72 @@ class html_form {
         return $post;
     }
 
-    function is_input_type( $type ) {
+    /**
+     *  Get a form field
+     *
+     *  @param string $id ID of field to return.
+     *
+     *  @return array|bool With fields properties, false on failure
+     */
+    public function get_field( $id ) {
+        if( isset( $this->fields[ $id ] ) ) {
+            return $this->fields[ $id ];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *  Get all form fields
+     *
+     *  @return array With all fields properties
+     */
+    public function get_all_fields() {
+        return $this->fields;
+    }
+
+    /**
+     *  Checks if is valid input type
+     *
+     *  @param string $type Type to check agains't valid types.
+     *
+     *  @return bool true if a vaild type, false on failure.
+     */
+    public function is_input_type( $type ) {
         return in_array( $type, $this->input_types );
     }
 
-    function update_type( array $key_values ) {
-        $args = func_get_args();
+    /**
+     *  Update an input type
+     *
+     *  @param array $key_values The key value pair of fields to update.
+     */
+    public function update_type( array $key_values ) {
+        foreach( $key_values as $k => $v ) {
+            if( isset( $this->fields[ $k ] ) ) {
+                $field = $this->fields[ $k ];
 
-        if( count( $args ) == 1 ) {
-            foreach( $key_values as $k => $v ) {
-                foreach( $this->fields as $fk => $fv ) {
-                    if( $fv[ 'name' ] == $k ) {
-                        $this->fields[ $fk ][ 'type' ] = $v;
-
-                        if( $this->is_input_type( $v ) )
-                            $this->fields[ $fk ] = array_merge( $this->input_defaults, $this->fields[ $fk ] );
-
-                        if( $v == 'textarea' )
-                            $this->fields[ $fk ] = array_merge( $this->textarea_defaults, $this->fields[ $fk ] );
-                    }
+                if( $field[ 'type' ] == $v ) {
+                    continue;
                 }
-            }
-        } else {
-            foreach( $key_values as $k ) {
-                foreach( $this->fields as $fk => $fv ) {
-                    if( $fv[ 'name' ] == $k ) {
-                        $this->fields[ $fk ][ 'type' ] = $args[ 1 ];
 
-                        if( $this->is_input_type( $args[ 1 ] ) )
-                            $this->fields[ $fk ] = array_merge( $this->input_defaults, $this->fields[ $fk ] );
+                $field[ 'type' ] = $v;
 
-                        if( $args[ 1 ] == 'textarea' )
-                            $this->fields[ $fk ] = array_merge( $this->textarea_defaults, $this->fields[ $fk ] );
-                    }
+                if( $this->is_input_type( $v ) ) {
+                    $this->fields[ $k ] = $this->input_defaults( $field );
+                } else if( $v == 'textarea' ) {
+                    $this->fields[ $k ] = $this->textarea_defaults( $field );
                 }
             }
         }
     }
 
-    function update_values( array $key_values ) {
+    /**
+     *  Update form field values
+     *
+     *  @param array $key_values The key value pairs of fields to update.
+     */
+    public function update_values( array $key_values ) {
         foreach( $key_values as $k => $v ) {
             foreach( $this->fields as $fk => $fv ) {
                 if( $fv[ 'name' ] == $k ) {
@@ -499,27 +565,52 @@ class html_form {
         }
     }
 
-    function input_defaults( $options ) {
+    /**
+     *  Returns new input properties after merging options with defaults
+     *
+     *  @param string|array The options to merge with defaults.
+     *
+     *  @return array With new input properties.
+     */
+    public function input_defaults( $options ) {
         if( is_string( $options ) )
             $options = array( 'name' => $options );
 
         if( ! is_array( $options ) )
             return;
+        
+        $options = array_intersect_key( $options, $this->input_defaults );
 
         return array_merge( $this->input_defaults, $options );
     }
 
-    function textarea_defaults( $options ) {
+    /**
+     *  Returns new textarea properties after merging options with defaults
+     *
+     *  @param string|array The options to merge with defaults.
+     *
+     *  @return array With new textarea properties.
+     */
+    public function textarea_defaults( $options ) {
         if( is_string( $options ) )
             $options = array( 'name' => $options );
 
         if( ! is_array( $options ) )
             return;
+        
+        $options = array_intersect_key( $options, $this->textarea_defaults );
+
+        unset( $options[ 'type' ] );
 
         return array_merge( $this->textarea_defaults, $options );
     }
 
-    function form_start() {
+    /**
+     *  Starting the HTML version of the form
+     *
+     *  @return string The opening part of the part.
+     */
+    public function form_start() {
         $form = '<form';
 
         if( $this->id )
@@ -541,18 +632,35 @@ class html_form {
         return $form;
     }
 
-    function form_end() {
+
+    /**
+     *  Closing off the HTML form
+     *
+     *  @return string The closing form HTML with the submit button.
+     */
+    public function form_end() {
         $submit = $this->get_input( $this->submit_defaults );
 
         return $submit . '</form>';
     }
 
-    function set_action( $value ) {
+
+    /**
+     *  Set the form submit action (submit url)
+     *
+     *  @param string $value The url to submit to.
+     */
+    public function set_action( $value ) {
         $this->action = $value;
     }
 
-    function set_method( $value ) {
-        if( strtolower( $value ) == 'post' || strtolower( $value ) == 'get' ) {
+    /**
+     *  Set the form submit method
+     *
+     *  @param string $value POST or GET method.
+     */
+    public function set_method( $value ) {
+        if( strtolower( $value ) === 'post' || strtolower( $value ) === 'get' ) {
             $this->method = $value;
         }
     }
