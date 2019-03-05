d85d1284325a776f7fb7bcdfa7583236b8bc48c7
Thapelo Moeti <benzitto@gmail.com>
2019-03-05 15:02:13 +0200

    added mission statement

---
+++ widgets/mission-statement/mission-statement.php
@@ -0,0 +1,13 @@
+<?php
+
+class TDT_HW_Mission_Statement extends TDT_HW_Widget_Base {
+
+    protected $id = 'mission_statement';
+
+    protected $meta = array(
+        array(
+            'name' => 'statement',
+            'type' => 'textarea'
+        )
+    );
+}
---
+++ widgets/mission-statement/tmpl/default.php
@@ -0,0 +1,6 @@
+
+<div class="hw-widget mission-statement" id="<?php echo "post-{$post->ID}"; ?>">
+    <?php
+        echo '<p>', $post->statement, '</p>';
+    ?>
+</div>
