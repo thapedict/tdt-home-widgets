42ee25b262163286aba4f7c0f1f0fd4d988e8bcc
Thapelo Moeti <benzitto@gmail.com>
2019-03-07 18:29:15 +0200

    Big Update.
    1. Added base_id
    2. register_shortcode & handle_shortcode functions
    3. modified load_one function
    4. load_widget_template function
    5. get_metas function
    6.  include_frontend_scripts function
    8. get_path & get_url functions

--- inc/class-tdt-hw-widget-base.php
+++ inc/class-tdt-hw-widget-base.php
@@ -7,7 +7,7 @@
  */
 
 /**
- *  Base class of how all the TDT_HW widgets should behave.
+ *  Base class of how all the TDT_HW widgets (admin backend objects) should behave.
  */
 abstract class TDT_HW_Widget_Base {
     /**
@@ -16,6 +16,13 @@ abstract class TDT_HW_Widget_Base {
      * @var string $id
      */
     protected $id;
+
+    /**
+     *  The hyphend id
+     *
+     *  @var string $base_id
+     */
+    protected $base_id;
 
     /**
      * The singular name of the Widget
@@ -44,8 +51,6 @@ abstract class TDT_HW_Widget_Base {
     public function __construct() {
         $this->set_names();
         $this->init();
-
-        // add_action( 'init', array( $this, 'init' ) );
     }
 
     /**
@@ -73,7 +78,7 @@ abstract class TDT_HW_Widget_Base {
         $this->register_post_type();
         $this->register_widget();
         $this->register_shortcode();
-        
+
         add_action( 'admin_menu', array( $this, 'admin_menu' ) );
         add_action( 'tdt_hw_notices', array( $this, 'show_notices' ) );
     }
@@ -124,11 +129,12 @@ abstract class TDT_HW_Widget_Base {
      *
      *  @return string url to current widget_base
      */
-    public function get_admin_url( $args = array() ) {
+    public function get_admin_url( array $args = array() ) {
         $_args = array( 'page' => $this->get_slug() );
 
-        if( ! is_array( $args ) )
+        if ( ! is_array( $args ) ) {
             $args = array();
+        }
 
         $query_args = array_merge( $args, $_args );
 
@@ -172,6 +178,10 @@ abstract class TDT_HW_Widget_Base {
      *  Creating names out of ID
      */
     protected function set_names() {
+        if ( ! $this->base_id ) {
+            $this->base_id = str_replace( '_', '-', $this->id );
+        }
+
         if ( ! $this->singular_name )
             $this->singular_name = ucwords( str_replace( '_', ' ', $this->id ) );
 
@@ -204,7 +214,40 @@ abstract class TDT_HW_Widget_Base {
      *  Register as a shortcode
      */
     protected function register_shortcode() {
+        add_shortcode( $this->id, array( $this, 'handle_shortcode' ) );
+    }
 
+    /**
+     *  The shortcode handler
+     *
+     *  @param array $attr Passed shortcode attributes.
+     *
+     *  @return string formatted HTML after processing the shortcode
+     */
+    public function handle_shortcode( $attr, $content, $shortcode ) {
+        $default_args = array(
+            'id' => null,
+            'template' => ''
+        );
+
+        $args = wp_parse_args( $attr, $default_args );
+
+        if ( empty( $args[ 'id' ] ) ) {
+            return;
+        }
+
+        $post = $this->get_post( intval( $args[ 'id' ] ) );
+
+        ob_start();
+
+        if ( $post ) {
+            $this->load_widget_template( $post, $args[ 'template' ] );
+            $this->include_frontend_scripts( $this->id );
+        } else {
+            echo '<!-- ', $this->id, ': Post Not Found -->';
+        }
+
+        return ob_get_clean();
     }
 
     /**
@@ -269,20 +312,32 @@ abstract class TDT_HW_Widget_Base {
      *  @param int $id the id (int) of the post to edit (0 if we want to add new post).
      */
     public function load_one( $id ) {
-        $html_form = new html_form( array( 'id' => $this->get_slug() ) );
+        $html_form = new TDT_HW_HTML_Form( array( 'id' => $this->get_slug() ) );
 
-        $fields = array_merge( array( 'ID', 'post_title', 'post_content' ), $this->meta );
+        $auto_fields = array(
+            array(
+                'name' => 'ID',
+                'type' => 'hidden'
+            ),
+            array(
+                'name' => 'post_title',
+                'label' => __( 'Title', 'tdt-hw' )
+            )
+        ) ;
 
-        $html_form->add_input( $fields );
+        $fields = array_merge( $auto_fields, $this->meta );
 
-        $html_form->update_type( array( 'ID' => 'hidden', 'post_content' => 'textarea' ) );
+        $html_form->add_field( $fields );
+
+        $widget_metas = $this->get_metas();
+        $widget_meta_names = $this->get_metas( true );
 
         if ( empty( $_POST ) ) {
             if ( $id ) {
                 $the_post = (array) $this->get_post( $id );
                 $the_meta = array();
 
-                foreach ( $this->meta as $m ) {
+                foreach ( $widget_meta_names as $m ) {
                     $the_meta[ $m ] = get_post_meta( $id, $m, true );
                 }
 
@@ -292,9 +347,15 @@ abstract class TDT_HW_Widget_Base {
         } else {
             $submitted = $html_form->get_post();
 
+            $submitted = stripslashes_deep( $submitted );
+
             $post_args = $this->get_post_args( $submitted );
 
-            $_id = wp_insert_post( $post_args, true );
+            if ( ! empty( $post_args[ 'ID' ] ) ) {
+                $_id = wp_update_post( $post_args, true );
+            } else {
+                $_id = wp_insert_post( $post_args, true );
+            }
 
             if ( is_int( $_id ) ) {
                 if ( ! $id ) { // creating a new post
@@ -304,12 +365,11 @@ abstract class TDT_HW_Widget_Base {
 
                 $html_form->update_values( $post_args );
 
-                $submitted_meta = array_intersect_key( $submitted, array_flip( $this->meta ) );
+                $submitted_meta = array_intersect_key( $submitted, $html_form->get_field_names( $widget_metas, true ) );
 
                 foreach ( $submitted_meta as $k => $v ) {
                     update_post_meta( $id, $k, $v );
                 }
-
                 $html_form->update_values( $submitted_meta );
 
                 $this->add_notice( __( 'Updated', 'tdt-hw' ) );
@@ -368,7 +428,7 @@ abstract class TDT_HW_Widget_Base {
             )
         );
 
-        $html_form->add_input( $inputs );
+        $html_form->add_field( $inputs );
 
         HTMLER::h2_e( __( 'Are you sure you want to delete this?', 'tdt-hw' ) );
 
@@ -377,6 +437,31 @@ abstract class TDT_HW_Widget_Base {
         $this->the_post_details( $id, $filter );
 
         $html_form->print_form();
+    }
+
+    /**
+     *  Loading the required widget template
+     *
+     *  @param WP_Post $post The CPT to use in the template.
+     *  @param string $template The template to use.
+     */
+    public function load_widget_template( $post, $template = '' ) {
+        $theme_override = '';
+        // check theme overrides
+        $theme_folder = trailingslashit( get_template_directory() ) . 'home-widgets' . DIRECTORY_SEPARATOR;
+
+        // 1. We first check if there's a theme override template for that specific post ID
+        // 2. Then check for a theme override template with the matching template name
+        // 3. Then lastly check if a theme override template with the widget name exists
+        if ( file_exists( $theme_folder . "{$this->base_id}-{$post->ID}.php" ) ) {
+            require $theme_folder . "{$this->base_id}-{$post->ID}.php";
+        } elseif ( file_exists( $theme_folder . "{$this->base_id}-{$template}.php" ) ) {
+            require $theme_folder . "{$this->base_id}-{$template}.php";
+        } elseif ( file_exists( $theme_folder . $this->base_id . '.php' ) ) {
+            require $theme_folder . $this->base_id . '.php';
+        } else {
+            require TDT_HW_WIDGETS_PATH . DIRECTORY_SEPARATOR . $this->base_id . DIRECTORY_SEPARATOR . 'tmpl/default.php';
+        }
     }
 
     /**
@@ -471,7 +556,7 @@ abstract class TDT_HW_Widget_Base {
             'post_title' => '',
             'post_content' => '',
             'post_status' => 'publish',
-            'ping_status' => 'closed'
+            'ping_status' => 'closed',
         );
 
         if ( ! empty( $args ) ) {
@@ -496,12 +581,12 @@ abstract class TDT_HW_Widget_Base {
     /**
      *  Prints outs the add new link
      */
-    public function the_add_new_button() {
+    public function the_add_new_button() { 
         $attr = array(
             'href' => $this->get_edit_url( 0 ),
             'class' => 'add-new-button button'
         );
-        
+
         HTMLER::a_e( __( 'Add New', 'tdt-hw' ), $attr );
     }
 
@@ -559,5 +644,53 @@ abstract class TDT_HW_Widget_Base {
         echo '</header>';
 
         do_action( 'tdt_hw_notices' );
+    }
+
+    /**
+     *  Extract metas from meta list
+     *
+     *  @param bool $names_only Whether to only get names.
+     *
+     *  @return array $list of widget metas.
+     */
+    public function get_metas( $names_only = false ) {
+        $metas = array();
+
+        $html_form = new html_form;
+
+        if ( $names_only ) {
+            $metas = $html_form->get_field_names( $this->meta );
+        } else {
+            $metas = $html_form->array_field_fixer( $this->meta );
+        }
+
+        return $metas;
+    }
+
+    /**
+     *  Include frontend styles and scripts.
+     */
+    public function include_frontend_scripts() {
+        if ( file_exists( $this->get_path() . '/assets/css/main.css' ) ) {
+            wp_enqueue_style( $this->base_id, $this->get_url() . 'assets/css/main.css', null, '1.0.1' );
+        }
+    }
+
+    /**
+     *  Get path of current widget.
+     *
+     *  @return string Current path.
+     */
+    public function get_path() {
+        return TDT_HW_WIDGETS_PATH . DIRECTORY_SEPARATOR . $this->base_id . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     *  Get url of current widget.
+     *
+     *  @return string Current url.
+     */
+    public function get_url() {
+        return TDT_HW_WIDGETS_URL . "{$this->base_id}/";
     }
 }
